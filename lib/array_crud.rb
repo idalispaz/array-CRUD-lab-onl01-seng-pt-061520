524a565b09350e6c1d4104985a9fe8371c6f74c7
Idalis Paz <idalispaz@gmail.com>
2020-06-23 17:15:19 +0000

    Automatically backed up by Learn

--- lib/array_crud.rb
+++ lib/array_crud.rb
@@ -28,7 +28,7 @@ end
 
 def retrieve_element_from_index(array, index_number)
   array_0 = ["wow","Miku","Matcha","Stella","Mr.Pie","arrays!"]
-  array_0[1]
+  array_0[2]
 end 
 
 def retrieve_first_element_from_array(array)
