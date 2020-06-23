9d67c9e708e7a092c492150856cdfe467ad4c2d9
Idalis Paz <idalispaz@gmail.com>
2020-06-23 16:59:01 +0000

    Automatically backed up by Learn

--- lib/array_crud.rb
+++ lib/array_crud.rb
@@ -9,6 +9,7 @@ end
 def add_element_to_end_of_array(array, element)
 array_0 = ["Miku", "Matcha", "Stella", "Mr.Pie"]
 array_0.push = ("arrays!")
+end
 def add_element_to_start_of_array(array, element)
   
 end
