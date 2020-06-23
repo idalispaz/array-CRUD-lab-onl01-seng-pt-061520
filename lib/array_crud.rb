e3f23f45eac295e4c0f1892578f20c4dde2b5e7a
Idalis Paz <idalispaz@gmail.com>
2020-06-23 17:05:31 +0000

    Automatically backed up by Learn

--- lib/array_crud.rb
+++ lib/array_crud.rb
@@ -13,7 +13,7 @@ end
 
 def add_element_to_start_of_array(array, element)
   array_0 = ["Miku", "Matcah", "Stella", "Mr.Pie"]
-  array_0.unshift("Harley")
+  array_0.unshift("wow")
 end
 
 def remove_element_from_end_of_array(array)
