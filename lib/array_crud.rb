755b110f65371d0d9192aebf5f6e3a5cc57ff05a
Idalis Paz <idalispaz@gmail.com>
2020-06-23 17:10:31 +0000

    Automatically backed up by Learn

--- lib/array_crud.rb
+++ lib/array_crud.rb
@@ -17,8 +17,8 @@ def add_element_to_start_of_array(array, element)
 end
 
 def remove_element_from_end_of_array(array)
-  array_0 = ["Miku","Matcha","Stella","Mr.Pie"]
-  array_0.pop
+  array_0 = ["Miku","Matcha","Stella","Mr.Pie","arrays!"]
+  array_0.pop("arrays!")
 end
 
 def remove_element_from_start_of_array(array)
